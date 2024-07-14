9f84bcfad80a6cb2667e10f58c3788dc2f56a8c5
Nguyễn Văn Đức <150463306+nguyenduc07082004@users.noreply.github.com>
2024-07-14 23:37:30 +0700

    main

--- W3-shop/src/component/ProductDetail.tsx
+++ W3-shop/src/component/ProductDetail.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Grid, Paper, Typography, Button } from '@mui/material';
-<<<<<<< HEAD
 import { Products } from '../type/Interface'; // Thay đổi path tới types
-=======
-import { Products } from '../type/Interface';
 
->>>>>>> 33175e6fd265e02293da4308918fa8708baaf9c5
+
 
 interface ProductDetailProps {
   products: Products[]; // Thay đổi kiểu dữ liệu nếu cần
