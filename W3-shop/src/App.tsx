b120c317eb8160c1a19e0df4506d3833b72e56d5
quangvt12 <quangtranvinh802@gmail.com>
2024-07-15 21:51:36 +0700

    main

--- W3-shop/src/App.tsx
+++ W3-shop/src/App.tsx
@@ -1,42 +1,20 @@
 import './App.css'
 import Home from './component/Home'
-import {useRoutes } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 import Admin from './page/admin/Admin';
 import Register from './page/SubmitAdmin/Regiter';
 import Login from './page/SubmitAdmin/Login';
 import ProductDetail from './component/ProductDetail';
 
-const routeConfig = [
-  {
-    path: "admin",
-    element: <Admin/>,
-  },
-  {
-    path: "/",
-    element: <Home />,
-    children: [
-      {
-        path: "register",
-        element: <Register />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "/product/:id",
-        element: <ProductDetail />,
-      },
-    ],
-  },
-];
 function App() {
-  const router=useRoutes(routeConfig)
-
-  
-
   return (
-     <main>{router}</main>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetail/>}/>
+      <Route path="/admin" element={<Admin/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+     </Routes>
   )
 }
 
