3e544fc71dde8cfe0aa7395255c05deb7e97fbfa
Nguyễn Văn Đức <150463306+nguyenduc07082004@users.noreply.github.com>
2024-07-15 21:26:56 +0700

    main

--- W3-shop/src/main.tsx
+++ W3-shop/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './component/Header.tsx'
+import  Footer  from "./component/Footer.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Header />
     <App />
+    <Footer />
     </BrowserRouter>
   </React.StrictMode>,
 )
