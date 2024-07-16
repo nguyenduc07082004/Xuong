007dc22b42e45e6ceff8d28fb24aca692736d938
quangvt12 <quangtranvinh802@gmail.com>
2024-07-16 07:41:45 +0700

    main

--- W3-shop/src/page/SubmitAdmin/Login.tsx
+++ W3-shop/src/page/SubmitAdmin/Login.tsx
@@ -1,136 +1,128 @@
 import {
-    Avatar,
-    Box,
-    Button,
-    Checkbox,
-    CssBaseline,
-    FormControlLabel,
-    Grid,
-    Paper,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  import { Link } from "react-router-dom";
-  import { useForm, SubmitHandler } from "react-hook-form"
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { getLogin } from "../../component/Axios/axios";
-  
-  type RegisterFormParams = {
-    email: string;
-    password: string;
-  };
-  
-  
-  const Login = () => {
-    const navigate = useNavigate() ; 
-  
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<RegisterFormParams>()
-    const onSubmit: SubmitHandler<RegisterFormParams> = async () => {
-      try {
-        getLogin;
-        alert("Login successful")
-        navigate('/')
-      } catch (error) {
-        alert(error)
-      }
-    }
-  
-    return (
-      <>
-        <Grid container component="main" justifyContent={"center"}>
-          <CssBaseline />
-          <Grid
-            item
-            xs={12}
-            sm={8}
-            md={5}
-            component={Paper}
-            elevation={1}
-            square
-            p={5}
-          >
-            <div>
-              <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <Avatar></Avatar>
-              </Box>
-              <Typography component="h1" variant="h5" my={2} textAlign={"center"}>
-                Login
-              </Typography>
-              <form onSubmit={handleSubmit(onSubmit)}>
 
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  label="Email"
-                  type="email"
-                  id="email"
-                  autoComplete="current-email"
-                  {...register("email", {
-                    required: "Email is required",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                      message: "invalid email address",
-                    },
-                  })}
-                  error={!!errors?.email?.message}
-                  helperText={errors?.email?.message}
-                  name="email"
-  
-                />
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  {...register("password", {
-                    required: "Password is required",
-                    minLength: {
-                      value: 6,
-                      message: "Password is min length 6 characters",
-                    },
-                  })}
-                  error={!!errors?.password?.message}
-                  helperText={errors?.password?.message}
-  
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                >
-                  Sign In
-                </Button>
-                <Grid container>
-                  <Grid item mt={5}>
-                    <Typography variant="body2">
-                      {"Don't have an account?"}
-                      <Link to="/register">Sign Up</Link>
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </form>
-            </div>
-          </Grid>
-        </Grid>
-      </>
-    );
+type RegisterFormParams = {
+  email: string;
+  password: string;
+};
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormParams>();
+
+  const onSubmit: SubmitHandler<RegisterFormParams> = async (data) => {
+    try {
+      await getLogin(data); // Gọi hàm getLogin với dữ liệu từ form
+      alert("Đăng nhập thành công");
+      navigate('/');
+    } catch (error) {
+      alert(error);
+    }
   };
-  
-  export default Login;
-  
+
+  return (
+    <>
+      <Grid container component="main" justifyContent={"center"}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={1}
+          square
+          p={5}
+        >
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Avatar />
+          </Box>
+          <Typography component="h1" variant="h5" my={2} textAlign={"center"}>
+            Đăng nhập
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="Email"
+              type="email"
+              id="email"
+              autoComplete="current-email"
+              {...register("email", {
+                required: "Email là bắt buộc",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Địa chỉ email không hợp lệ",
+                },
+              })}
+              error={!!errors?.email?.message}
+              helperText={errors?.email?.message}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="Mật khẩu"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              {...register("password", {
+                required: "Mật khẩu là bắt buộc",
+                minLength: {
+                  value: 6,
+                  message: "Mật khẩu phải có ít nhất 6 ký tự",
+                },
+              })}
+              error={!!errors?.password?.message}
+              helperText={errors?.password?.message}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Nhớ tôi"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Đăng nhập
+            </Button>
+            <Grid container>
+              <Grid item mt={5}>
+                <Typography variant="body2">
+                  {"Bạn chưa có tài khoản? "}
+                  <Link to="/register">Đăng ký</Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Login;
--- W3-shop/src/page/SubmitAdmin/Regiter.tsx
+++ W3-shop/src/page/SubmitAdmin/Regiter.tsx
@@ -10,163 +10,128 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { getRegister } from "../../component/Axios/axios";
 import { Email } from "../../type/Interface";
 import { Link } from "react-router-dom";
 
-
-
-
 const Register = () => {
-  const navigate = useNavigate() ; 
+  const navigate = useNavigate();
 
-  let confirmPassword = ""
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Email>()
+  } = useForm<Email>();
 
-  confirmPassword = watch("confirmPassword", "");
-  
+  const password = watch("password", "");
 
-  const onSubmit: SubmitHandler<Email> = async () => {
+  const onSubmit: SubmitHandler<Email> = async (data) => {
     try {
-      getRegister;
-      alert("Register successful")
-      navigate("/login")
+      await getRegister(data); // Giả sử getRegister là một hàm nhận dữ liệu đăng ký
+      alert("Đăng ký thành công");
+      navigate("/login");
     } catch (error) {
-      alert(error)
+      alert(error);
     }
-  }
+  };
 
   return (
-    <>
-      <Grid container component="main" justifyContent={"center"}>
-        <CssBaseline />
-        {/* <Grid item xs={false} sm={4} md={7} className={classes.image} /> */}
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={5}
-          component={Paper}
-          elevation={1}
-          square
-          p={5}
-        >
-          <div>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Avatar></Avatar>
-            </Box>
-            <Typography component="h1" variant="h5" my={2} textAlign={"center"}>
-              Register
-            </Typography>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                autoFocus
-                {...register("username", {
-                  required: "Username is required",
-                })}
-                error={!!errors?.username?.message}
-                helperText={errors?.username?.message}
-                name="username"
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                label="Email"
-                type="email"
-                id="email"
-                autoComplete="current-email"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: "invalid email address",
-                  },
-                })}
-                error={!!errors?.email?.message}
-                helperText={errors?.email?.message}
-                name="email"
-
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                {...register("password", {
-                  required: "Password is required",
-                  minLength: {
-                    value: 6,
-                    message: "Password is min length 6 characters",
-                  },
-                })}
-                error={!!errors?.password?.message}
-                helperText={errors?.password?.message}
-
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                {...register("confirmPassword", {
-                  validate: value =>
-                    value == confirmPassword || "The passwords do not match"
-                })}
-                error={!!errors?.confirmPassword?.message}
-                helperText={errors?.confirmPassword?.message}
-
-                name="confirmPassword"
-                label="Confirm Password"
-                type="password"
-                id="confirmPassword"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Sign Up
-              </Button>
-              <Grid container>
-                <Grid item mt={5}>
-                  <Typography variant="body2">
-                    {"Don't have an account?"}
-                    <Link to="/login">Login</Link>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </form>
-          </div>
-        </Grid>
+    <Grid container component="main" justifyContent="center">
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={1} square p={5}>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Avatar />
+        </Box>
+        <Typography component="h1" variant="h5" my={2} textAlign="center">
+          Đăng ký
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Tên đăng nhập"
+            autoFocus
+            {...register("username", { required: "Tên đăng nhập là bắt buộc" })}
+            error={!!errors?.username?.message}
+            helperText={errors?.username?.message}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Email"
+            type="email"
+            id="email"
+            autoComplete="current-email"
+            {...register("email", {
+              required: "Email là bắt buộc",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Địa chỉ email không hợp lệ",
+              },
+            })}
+            error={!!errors?.email?.message}
+            helperText={errors?.email?.message}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Mật khẩu"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            {...register("password", {
+              required: "Mật khẩu là bắt buộc",
+              minLength: {
+                value: 6,
+                message: "Mật khẩu phải có ít nhất 6 ký tự",
+              },
+            })}
+            error={!!errors?.password?.message}
+            helperText={errors?.password?.message}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Xác nhận mật khẩu"
+            type="password"
+            id="confirmPassword"
+            autoComplete="current-password"
+            {...register("confirmPassword", {
+              validate: (value) => value === password || "Mật khẩu không khớp",
+            })}
+            error={!!errors?.confirmPassword?.message}
+            helperText={errors?.confirmPassword?.message}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Nhớ tôi"
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Đăng ký
+          </Button>
+          <Grid container>
+            <Grid item mt={5}>
+              <Typography variant="body2">
+                {"Bạn đã có tài khoản? "}
+                <Link to="/login">Đăng nhập</Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </form>
       </Grid>
-    </>
+    </Grid>
   );
 };
 
